d44e2c0fdc678e4183cfcbdbcde2d11d8d1f2fa1
Timothy Jones <t.jones@aptacode.com>
2020-05-30 20:45:15 +0100

    Added Generic EFCore Repo & WebApi Controller

---
+++ Aptacode.NetCore/Controllers/GenericController.cs
@@ -0,0 +1,71 @@
+﻿using Aptacode.CSharp.Utilities.Persistence;
+using Aptacode.CSharp.Utilities.Persistence.Repository;
+using Aptacode.CSharp.Utilities.Persistence.UnitOfWork;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aptacode.NetCore
+{
+    public abstract class GenericController<TEntity> : ControllerBase where TEntity : IEntity
+    {
+        protected readonly IRepository<TEntity> Repository;
+        protected readonly IGenericUnitOfWork UnitOfWork;
+
+        protected GenericController(IGenericUnitOfWork unitOfWork)
+        {
+            UnitOfWork = unitOfWork;
+            Repository = unitOfWork.Repository<TEntity>();
+        }
+
+        [HttpPut]
+        public virtual async Task<IActionResult> Create()
+        {
+            var bodyContent = await new StreamReader(Request.Body).ReadToEndAsync().ConfigureAwait(false);
+
+            var newEntity = JsonConvert.DeserializeObject<TEntity>(bodyContent);
+
+            await Repository.Create(newEntity).ConfigureAwait(false);
+            await UnitOfWork.Commit().ConfigureAwait(false);
+
+            return Ok(newEntity.Id);
+        }
+
+        [HttpGet]
+        public virtual async Task<IActionResult> ReadAll()
+        {
+            return Ok(Repository.AsQueryable().Select(e => e.Id));
+        }
+
+        [HttpGet("{id}")]
+        public virtual async Task<IActionResult> Read(int id)
+        {
+            var entity = await Repository.Get(id).ConfigureAwait(false);
+            return Ok(entity);
+        }
+
+        [HttpPost]
+        public virtual async Task<IActionResult> Update()
+        {
+            var bodyContent = await new StreamReader(Request.Body).ReadToEndAsync().ConfigureAwait(false);
+
+            var newEntity = JsonConvert.DeserializeObject<TEntity>(bodyContent);
+
+            await Repository.Update(newEntity).ConfigureAwait(false);
+            await UnitOfWork.Commit().ConfigureAwait(false);
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public virtual async Task<IActionResult> Delete(int id)
+        {
+            await Repository.Delete(id).ConfigureAwait(false);
+            await UnitOfWork.Commit().ConfigureAwait(false);
+
+            return Ok();
+        }
+    }
+}
---
+++ Aptacode.NetCore/Persistence/GenericEfRepository.cs
@@ -0,0 +1,52 @@
+﻿using Aptacode.CSharp.Utilities.Persistence;
+using Aptacode.CSharp.Utilities.Persistence.Repository;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aptacode.NetCore.Persistence
+{
+    public abstract class GenericEfRepository<TEntity> : IRepository<TEntity> where TEntity : class, IEntity
+    {
+        protected readonly DbSet<TEntity> DbSet;
+
+        protected GenericEfRepository(DbSet<TEntity> dbSet)
+        {
+            DbSet = dbSet;
+        }
+
+        public virtual async Task<int> Create(TEntity entity)
+        {
+            DbSet.Add(entity);
+            return 0;
+        }
+
+        public virtual async Task Update(TEntity entity)
+        {
+            DbSet.Attach(entity);
+        }
+
+        public virtual async Task<IEnumerable<TEntity>> GetAll()
+        {
+            return await DbSet.ToListAsync().ConfigureAwait(false);
+        }
+
+        public virtual async Task<TEntity> Get(int id)
+        {
+            return await DbSet.FindAsync(id).ConfigureAwait(false);
+        }
+
+        public virtual async Task Delete(int id)
+        {
+            var entity = await Get(id).ConfigureAwait(false);
+            DbSet.Remove(entity);
+
+        }
+
+        public IQueryable<TEntity> AsQueryable()
+        {
+            return DbSet.AsQueryable();
+        }
+    }
+}
