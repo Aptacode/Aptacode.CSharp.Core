8e8e97b4643e4522f40f05f136ba5c98d6dd5801
Timothy Jones <t.jones@aptacode.com>
2020-06-02 17:09:29 +0100

    Removed BearerGenericController

---
+++ Aptacode.CSharp.Core/Controllers/ControllerUtilities.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+
+namespace Aptacode.CSharp.Core.Controllers
+{
+    public static class ControllerUtilities
+    {
+        public static int GetId(ClaimsPrincipal user)
+        {
+            var userId = user.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+            if (int.TryParse(userId, out var result))
+            {
+                return result;
+            }
+
+            throw new ArgumentException("Invalid user token");
+        }
+
+        public static string GetUserRole(ClaimsPrincipal user)
+        {
+            var userRole = user.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role)?.Value;
+            if (string.IsNullOrEmpty(userRole))
+            {
+                throw new ArgumentException("Invalid user token");
+            }
+
+            return userRole;
+        }
+    }
+}
